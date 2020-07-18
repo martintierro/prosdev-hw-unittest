83e20a7c3c19622750062ac23e895b2c64b92ee6
Martin Tierro <martintierro15@gmail.com>
2020-07-18 14:41:11 +0800

    Added code for gradeClassifier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 const functions = {
   gradeClassifier: (grade) => {
-    // TODO: Write code here
+    if(grade<=74.99){
+      console.log("D - Did not Meet Expectation")
+    }
+    else if(grade<=79.99){
+      console.log("N - Needs Improvement")
+    }
+    else if(grade<=84.99){
+      console.log("S - Satisfactory")
+    }
+    else if(grade<=89.99){
+      console.log("G - Good")
+    }
+    else if(grade<=94.99){
+      console.log("V - Very Good")
+    }
+    else if(grade<=100){
+      console.log("O - Outstanding")
+    }
+    else{
+      console.log("Not a valid grade")
+    }
   }
 }
 
